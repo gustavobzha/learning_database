487db1295e6cb8ab5fa6545ea9d2ef1c2a777fe7
Gustavo Bezerras <gustavobs16@gmail.com>
2021-07-08 06:43:12 -0400

    first commit

---
+++ dados.sql
@@ -0,0 +1,10 @@
+INSERT INTO livros VALUES ('Cavaleiro Real',	'Ana Claudia'	,'Feminino',	465,	'Atlas',	49.9	, 'RJ',	2009);
+INSERT INTO livros VALUES ('SQL para leigos',	'João Nunes',	'Masculino',	450,	'Addison',	98,	'SP',	2018);
+INSERT INTO livros VALUES ('Receitas Caseiras',	'Celia Tavares',	'Feminino',	210,	'Atlas',	45,	'RJ',	2008);
+INSERT INTO livros VALUES ('Pessoas Efetivas',	'Eduardo Santos',	'Masculino',	390,	'Beta',	78.99,	'RJ',	2018);
+INSERT INTO livros VALUES ('Habitos Saudáveis',	'Eduardo Santos',	'Masculino',	630,	'Beta',	150.98,	'RJ',	2019);
+INSERT INTO livros VALUES ('A Casa Marrom',	'Hermes Macedo',	'Masculino',	250,	'Bubba',	60,	'MG',	2016);
+INSERT INTO livros VALUES ('Estacio Querido',	'Geraldo Francisco',	'Masculino',	310,	'Insignia',	100,	'ES',	2015);
+INSERT INTO livros VALUES ('Pra sempre amigas',	'Leda Silva',	'Feminino',	510,	'Insignia',	78.98,	'ES',	2011);
+INSERT INTO livros VALUES ('Copas Inesqueciveis',	'Marco Alcantara',	'Masculino',	200,	'Larson',	130.98,	'RS',	2018);
+INSERT INTO livros VALUES ('O poder da mente',	'Clara Mafra',	'Feminino',	120,	'Continental',	56.58,	'SP',	2017);
